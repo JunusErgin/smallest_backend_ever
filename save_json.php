cd71e0ce13e4f103ef5932bb69f37a73c7cf1c5d
Junus Ergin <junus@developerakademie.com>
2020-09-24 14:20:16 +0200

    first commit

---
+++ save_json.php
@@ -0,0 +1,11 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+
+$json = file_get_contents('php://input');
+if (strlen($json) > 10000) {
+    die('Payload is too long (Max. 10000 characters).');
+}
+$file = 'my_json.json';
+file_put_contents($file, $json);
