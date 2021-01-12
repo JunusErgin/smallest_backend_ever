39fa14ded09ad2626e34602d3ed05150de2bb126
Junus Ergin <junus@developerakademie.com>
2021-01-12 16:30:41 +0100

    v2 created

---
+++ mini_backend.js
@@ -0,0 +1,127 @@
+let jsonFromServer = {};
+let BASE_SERVER_URL;
+
+const backend = {
+    setItem: function(key, item) {
+        jsonFromServer[key] = item;
+        return saveJSONToServer();
+    },
+    getItem: function(key) {
+        return jsonFromServer[key];
+    },
+    deleteItem: function(key) {
+        delete jsonFromServer[key];
+        return saveJSONToServer();
+    }
+};
+window.onload = async function() {
+    downloadFromServer();
+}
+
+async function downloadFromServer() {
+    let result = await loadJSONFromServer();
+    jsonFromServer = JSON.parse(result);
+    console.log('Loaded', result);
+}
+
+function setURL(url) {
+    BASE_SERVER_URL = url;
+}
+
+/**
+ * Saves a JSON or JSON Array to the Server
+ */
+function saveJSONToServer() {
+    return new Promise(function(resolve, reject) {
+        let xhttp = new XMLHttpRequest();
+        let proxy = determineProxySettings();
+        let serverURL = proxy + BASE_SERVER_URL + '/save_json.php';
+        xhttp.open('POST', serverURL);
+
+        xhttp.onreadystatechange = function(oEvent) {
+            if (xhttp.readyState === 4) {
+                if (xhttp.status >= 200 && xhttp.status <= 399) {
+                    resolve(xhttp.responseText);
+                } else {
+                    reject(xhttp.statusText);
+                }
+            }
+        };
+
+        console.log('Posten', jsonFromServer);
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.send(JSON.stringify(jsonFromServer));
+
+    });
+}
+
+
+
+
+
+
+/**
+ * Loads a JSON or JSON Array to the Server
+ * payload {JSON | Array} - The payload you want to store
+ */
+function loadJSONFromServer() {
+    return new Promise(function(resolve, reject) {
+        let xhttp = new XMLHttpRequest();
+        let proxy = determineProxySettings();
+        let serverURL = proxy + BASE_SERVER_URL + '/my_json.json?noache=' + (new Date().getTime());
+        xhttp.open('GET', serverURL);
+
+        xhttp.onreadystatechange = function(oEvent) {
+            if (xhttp.readyState === 4) {
+                if (xhttp.status >= 200 && xhttp.status <= 399) {
+                    resolve(xhttp.responseText);
+                } else {
+                    reject(xhttp.statusText);
+                }
+            }
+        };
+
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.send();
+
+    });
+}
+
+
+
+
+
+/**
+ * Saves a JSON or JSON Array to the Server
+ */
+function saveJSONToServer() {
+    return new Promise(function(resolve, reject) {
+        let xhttp = new XMLHttpRequest();
+        let proxy = determineProxySettings();
+        let serverURL = proxy + BASE_SERVER_URL + '/save_json.php';
+        xhttp.open('POST', serverURL);
+
+        xhttp.onreadystatechange = function(oEvent) {
+            if (xhttp.readyState === 4) {
+                if (xhttp.status >= 200 && xhttp.status <= 399) {
+                    resolve(xhttp.responseText);
+                } else {
+                    reject(xhttp.statusText);
+                }
+            }
+        };
+
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.send(JSON.stringify(jsonFromServer));
+
+    });
+}
+
+
+function determineProxySettings() {
+    if (window.location.href.indexOf('.developerakademie.com') > -1) {
+        return '';
+    } else {
+        return 'https://cors-anywhere.herokuapp.com/';
+    }
+}
