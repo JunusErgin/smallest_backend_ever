1bcc6f8e7311425bdfd25b148390f90ce02cc7db
Junus Ergin <JunusErgin@users.noreply.github.com>
2021-02-28 22:34:49 +0100

    Update save_json.php

--- save_json.php
+++ save_json.php
@@ -7,5 +7,5 @@ $json = file_get_contents('php://input');
 if (strlen($json) > 10000) {
     die('Payload is too long (Max. 10000 characters).');
 }
-$file = 'my_json.json';
+$file = 'database.json';
 file_put_contents($file, $json);
