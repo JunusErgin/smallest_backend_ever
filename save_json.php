b9c2430a8ac4ac22b40efd9f068577f6017aae76
Junus Ergin <JunusErgin@users.noreply.github.com>
2022-10-06 15:14:10 +0200

    Update save_json.php

--- save_json.php
+++ save_json.php
@@ -4,8 +4,8 @@ header("Access-Control-Allow-Headers: *");
 
 
 $json = file_get_contents('php://input');
-if (strlen($json) > 10000) {
-    die('Payload is too long (Max. 10000 characters).');
+if (strlen($json) > 100000) {
+    die('Payload is too long (Max. 100000 characters).');
 }
 $file = 'database.json';
 file_put_contents($file, $json);
